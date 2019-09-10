16611af5f9a195f8ece330aa07c2b2082ed0c7d2
Lucas Varlesse <lucas.varlesse.da.silva@gmail.com>
2019-09-09 21:58:49 -0300

    reafactoring

--- lib/calls.ex
+++ lib/calls.ex
@@ -3,19 +3,48 @@ defmodule Calls do
 
   def main([csv_file_path]) do
     csv_file_path
-    |> File.stream!()
-    |> Enum.map(&create_task_to_calculate_call/1)
-    |> Enum.map(&Task.await/1)
+    |> parse_parameters()
+    |> create_and_calculate_calls()
     |> disregard_longer_call()
-    |> calculate_total_cost()
+    |> calculate_total_cost_of_calls()
     |> IO.puts()
   end
 
   def main(_args), do: IO.puts("Invalid arguments")
 
-  def create_task_to_calculate_call(call), do: Task.async(fn -> calculate_call(call) end)
+  def parse_parameters(csv_file_path) do
+    case File.read(csv_file_path) do
+      {:ok, content} ->
+        String.split(content, "\n")
 
-  def calculate_call(call) do
+      {:error, :enoent} ->
+        IO.puts("The file #{csv_file_path} does not exist")
+
+      {:error, :eisdir} ->
+        IO.puts("The named file #{csv_file_path} is a directory")
+
+      _ ->
+        IO.puts("Invalid file")
+    end
+  end
+
+  def create_and_calculate_calls(calls_parameters) do
+    if Enum.empty?(calls_parameters) do
+      IO.puts("The file has no calls")
+    else
+      calls_parameters
+      |> Enum.map(&proccess_call_async/1)
+      |> Enum.map(&Task.await/1)
+    end
+  end
+
+  def proccess_call_async(call) do
+    Task.async(fn ->
+      calculate_duration_and_cost_call(call)
+    end)
+  end
+
+  def calculate_duration_and_cost_call(call) do
     call
     |> create_call()
     |> Call.calcutate_duration()
@@ -23,51 +52,40 @@ defmodule Calls do
   end
 
   def create_call(
-    <<time_of_start::bytes-size(8)>> <> ";" <>
-    <<time_of_finish::bytes-size(8)>> <> ";" <>
-    <<call_from::bytes-size(13)>> <> ";" <>
-    <<call_to::bytes-size(13)>> <> "\n"
-    ) do
-    try_create_call(
-      time_of_start: time_of_start,
-      time_of_finish: time_of_finish,
-      call_from: call_from,
-      call_to: call_to
-    )
-  end
+        <<time_of_start::bytes-size(8)>> <>
+          ";" <>
+          <<time_of_finish::bytes-size(8)>> <>
+          ";" <>
+          <<call_from::bytes-size(13)>> <>
+          ";" <>
+          <<call_to::bytes-size(13)>>
+      ) do
+    try_create_call =
+      Call.new(
+        time_of_start: time_of_start,
+        time_of_finish: time_of_finish,
+        call_from: call_from,
+        call_to: call_to
+      )
 
-  def create_call(
-    <<time_of_start::bytes-size(8)>> <> ";" <>
-    <<time_of_finish::bytes-size(8)>> <> ";" <>
-    <<call_from::bytes-size(13)>> <> ";" <>
-    <<call_to::bytes-size(13)>>
-    ) do
-    try_create_call(
-      time_of_start: time_of_start,
-      time_of_finish: time_of_finish,
-      call_from: call_from,
-      call_to: call_to
-    )
+    case try_create_call do
+      {:ok, call} ->
+        call
+
+      {:error, :invalid_call} ->
+        IO.puts("Invalid call register")
+    end
   end
 
   def disregard_longer_call(calls) do
     calls
-    |> Enum.sort_by(&(&1.minutes_of_duration))
+    |> Enum.sort_by(& &1.minutes_of_duration)
     |> Enum.drop(-1)
   end
 
-  def calculate_total_cost(calls) do
+  def calculate_total_cost_of_calls(calls) do
     calls
-    |> Enum.map(&(&1.total_cost))
+    |> Enum.map(& &1.total_cost)
     |> Enum.sum()
-  end
-
-  defp try_create_call(params) do
-    case Call.new(params) do
-      {:ok, call} ->
-        call
-      {:error, :invalid_call} ->
-        IO.puts("Invalid call register")
-    end
   end
 end
--- lib/calls/call.ex
+++ lib/calls/call.ex
@@ -1,25 +1,27 @@
 defmodule Calls.Call do
   alias __MODULE__
 
-  @enforce_keys [:time_of_start, :time_of_finish, :call_from, :call_to] ++ [:minutes_of_duration, :total_cost]
+  @enforce_keys [:time_of_start, :time_of_finish, :call_from, :call_to]
+  @keys [:minutes_of_duration, :total_cost]
 
-  defstruct @enforce_keys
+  defstruct @enforce_keys ++ @keys
 
-  def new(params) when is_list(params) or is_map(params) do
-    case parse_params(params) do
+  def new(parameters) when is_list(parameters) or is_map(parameters) do
+    case parse_parameters(parameters) do
+      {:ok, parameters} ->
+        {:ok, struct(Call, parameters)}
+
       {:error, :invalid_call} ->
         {:error, :invalid_call}
-      params ->
-        {:ok, struct(Call, params)}
     end
   end
 
-  def new(_params), do: {:error, :invalid_call}
+  def new(_parameters), do: {:error, :invalid_call}
 
-  def calcutate_duration(%Call{time_of_start: time_of_start, time_of_finish: time_of_finish} = call) do
+  def calcutate_duration(%Call{} = call) do
     time_of_duration =
-      time_of_finish
-      |> Time.diff(time_of_start)
+      call.time_of_finish
+      |> Time.diff(call.time_of_start)
       |> convert_to_minutes()
 
     %Call{call | minutes_of_duration: time_of_duration}
@@ -29,22 +31,23 @@ defmodule Calls.Call do
     %Call{call | total_cost: cost_by_minute(minutes_of_duration)}
   end
 
-  defp cost_by_minute(minute) when minute <= 5, do: Float.floor(minute * 0.05, 2)
-
-  defp cost_by_minute(minute), do: Float.floor(0.25 + ((minute - 5) * 0.02), 2)
-
-  defp parse_params(params) do
+  defp parse_parameters(parameters) do
     try do
-      {:ok, time_of_start} = Time.from_iso8601(params[:time_of_start])
-      {:ok, time_of_finish} = Time.from_iso8601(params[:time_of_finish])
+      {:ok, time_of_start} = Time.from_iso8601(parameters[:time_of_start])
+      {:ok, time_of_finish} = Time.from_iso8601(parameters[:time_of_finish])
 
-      params = put_in(params[:time_of_start], time_of_start)
+      parameters = put_in(parameters[:time_of_start], time_of_start)
+      parameters = put_in(parameters[:time_of_finish], time_of_finish)
 
-      put_in(params[:time_of_finish], time_of_finish)
+      {:ok, parameters}
     catch
       _ -> {:error, :invalid_call}
     end
   end
 
   defp convert_to_minutes(seconds), do: Float.floor(seconds / 60, 2) |> trunc()
+
+  defp cost_by_minute(minute) when minute <= 5, do: Float.floor(minute * 0.05, 2)
+
+  defp cost_by_minute(minute), do: Float.floor(0.25 + (minute - 5) * 0.02, 2)
 end
